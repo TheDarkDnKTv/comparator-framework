4d741dbbc071b3287119f211d45211ac6709e7f6
TheDarkDnKTv <42381105+TheDarkDnKTv@users.noreply.github.com>
2026-02-17 14:13:58 +0900

    Add CommonJS build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['lib/index.ts'],
-    format: ['esm'],
+    format: ['esm', 'cjs'],
     dts: true,
     splitting: true,
     sourcemap: true,
